cf2035953a4c71c70d0914e9077c57ba12006a38
idosh97 <idoschwartz3@gmail.com>
2023-04-16 22:15:02 +0300

    1.2

--- Computer_Assignment_1.m
+++ Computer_Assignment_1.m
@@ -1,8 +1,10 @@
+clear;
 close all;
 %% 1.1 Data Generation
 
 [v_m,fs] = audioread("in-the-air.wav");
 %sound(v_m,fs);
+
 T_s = 1/fs;
 N = length(v_m);
 t = 0:T_s:(N-1)*T_s;
@@ -17,8 +19,27 @@ ylabel("v_m");
 xlabel("t");
 
 subplot(2,1,2);
-plot(f,V_m);
+plot(f,abs(V_m));
 grid on;
-ylabel("V_m");
+ylabel("|V_m|");
 xlabel("f");
 
+% evaluation of the bandwidth
+%bw = V_m>0.01*max(V_m);
+%BW = 1.1khz
+
+%% 1.2 Modulator
+fc = 15*10^3;  % carrier frequency
+k_AM = 0.02;  % modulation index
+
+v_AM = ammod(v_m,fc, fs);
+
+V_AM = fftshift(fft(v_AM))/sqrt(N);
+
+figure;
+plot(f,abs(V_AM));
+grid on;
+ylabel("V_AM");
+xlabel("f");
+
+sound(v_AM,fs); 
