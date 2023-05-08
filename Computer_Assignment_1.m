dfafd38b1028256c8d299fbe75702787b65f7ed9
idosh97 <idoschwartz3@gmail.com>
2023-05-08 14:39:26 +0300

    Q1

--- Computer_Assignment_1.m
+++ Computer_Assignment_1.m
@@ -4,7 +4,7 @@ clc;
 %% 1.1 Data Generation
 
 [v_m,fs] = audioread("in-the-air.wav");
-% sound(v_m,fs);
+sound(v_m,fs);
 
 T_s = 1/fs;
 N = length(v_m);
@@ -12,6 +12,7 @@ t = 0:T_s:(N-1)*T_s;
 f = linspace(-fs/2,fs/2,N);
 V_m = fftshift(fft(v_m))/sqrt(N);
 
+% 1.1.1
 figure;
 subplot(2,1,1);
 plot(t,v_m);
@@ -25,11 +26,10 @@ grid on;
 ylabel("V_m(f)");
 xlabel("f");
 
-% evaluation of the bandwidth
+% 1.1.2 evaluation of the bandwidth
 
 bw_v =find( V_m > 0.1*max(V_m) );
 bw = max(bw_v / (length(f)/2))*10^4;
-
 % bw = 0.7*10^4;
 %% 1.2 Modulator
 fc = 15*10^3;  % carrier frequency
@@ -112,10 +112,10 @@ legend('X_d(f)','V_m(f)');
 
 % sound(x_d,fs); %1.4.3
 
-correlation = xcorr(x_d,v_m,0,'coeff'); %1.4.4
+correlation(1) = xcorr(x_d,v_m,0,'coeff'); %1.4.4
 
 %% 1.5
-% 1.5.1
+% 1.5.1 - Repeat items 1.3 and 1.4, this time choosing N_0=0.02
 
 % 1.3 Channel
 N_0 = 0.02;
@@ -183,14 +183,160 @@ legend('X_d(f)','V_m(f)');
 
 sound(x_d,fs); %1.4.3
 
-correlation = xcorr(x_d,v_m,0,'coeff'); %1.4.4
+correlation(2) = xcorr(x_d,v_m,0,'coeff'); %1.4.4
 
-%% 1.5.2
+% %% 1.5.2
+% 
+% % 1.3 Channel
+% N_0 = 8*10^-4;
+% z = (sqrt(N_0/2)*randn(1,N)).';
+% x_r = v_AM + z;
+% X_r = fftshift(fft(x_r))/sqrt(N);
+% 
+% figure;
+% subplot(2,1,1);
+% plot(t,x_r);
+% grid on;
+% ylabel("x_r(t)");
+% xlabel("t");
+% 
+% subplot(2,1,2);
+% plot(f,X_r);
+% grid on;
+% ylabel("X_r(f)");
+% xlabel("f");
+% 
+% 
+% % 1.4 Demodulator
+% 
+% % 1.4.1
+% x_L = bandpass(x_r,[fc-bw fc+bw],fs);
+% X_L = fftshift(fft(x_L))/sqrt(N);
+% 
+% figure;
+% subplot(2,1,1);
+% plot(t,x_L);
+% grid on;
+% ylabel("x_L(t)");
+% xlabel("t");
+% 
+% subplot(2,1,2);
+% plot(f,X_L);
+% grid on;
+% ylabel("X_L(f)");
+% xlabel("f");
+% 
+% % 1.4.2
+% x_d = amdemod(x_L,fc,fs,0,k_AM);
+% X_d = fftshift(fft(lowpass(x_d,bw,fs)))/sqrt(N);
+% 
+% % X_d1 = fftshift(fft(x_d))/sqrt(N);
+% % X_d = lowpass(X_d1,bw,fs);
+% 
+% figure;
+% subplot(2,1,1);
+% plot(t,x_d);
+% hold on
+% plot(t,v_m);
+% ylabel("v_m(t) and x_d(t)");
+% xlabel("t");
+% legend('x_d(t)','v_m(t)');
+% 
+% subplot(2,1,2);
+% plot(f,X_d);
+% hold on 
+% plot(f,V_m);
+% grid on;
+% ylabel("V_m(f) and X_d(f)");
+% xlabel("f");
+% legend('X_d(f)','V_m(f)');
+% 
+% % sound(x_d,fs); %1.4.3
+% 
+% correlation = xcorr(x_d,v_m,0,'coeff'); %1.4.4
+% 
+% % 1.5
+% % 1.5.1
+% 
+% % 1.3 Channel
+% N_0 = 0.02;
+% z = (sqrt(N_0/2)*randn(1,N)).';
+% x_r = v_AM + z;
+% X_r = fftshift(fft(x_r))/sqrt(N);
+% 
+% figure;
+% subplot(2,1,1);
+% plot(t,x_r);
+% grid on;
+% ylabel("x_r(t)");
+% xlabel("t");
+% 
+% subplot(2,1,2);
+% plot(f,X_r);
+% grid on;
+% ylabel("X_r(f)");
+% xlabel("f");
+% 
+% 
+% % 1.4 Demodulator
+% 
+% % 1.4.1
+% x_L = bandpass(x_r,[fc-bw fc+bw],fs);
+% X_L = fftshift(fft(x_L))/sqrt(N);
+% 
+% figure;
+% subplot(2,1,1);
+% plot(t,x_L);
+% grid on;
+% ylabel("x_L(t)");
+% xlabel("t");
+% 
+% subplot(2,1,2);
+% plot(f,X_L);
+% grid on;
+% ylabel("X_L(f)");
+% xlabel("f");
+% 
+% % 1.4.2
+% x_d = amdemod(x_L,fc,fs,0,k_AM);
+% X_d = fftshift(fft(lowpass(x_d,bw,fs)))/sqrt(N);
+% 
+% % X_d1 = fftshift(fft(x_d))/sqrt(N);
+% % X_d = lowpass(X_d1,bw,fs);
+% 
+% figure;
+% subplot(2,1,1);
+% plot(t,x_d);
+% hold on
+% plot(t,v_m);
+% ylabel("v_m(t) and x_d(t)");
+% xlabel("t");
+% legend('x_d(t)','v_m(t)');
+% 
+% subplot(2,1,2);
+% plot(f,X_d);
+% hold on 
+% plot(f,V_m);
+% grid on;
+% ylabel("V_m(f) and X_d(f)");
+% xlabel("f");
+% legend('X_d(f)','V_m(f)');
+% 
+% sound(x_d,fs); %1.4.3
+% 
+% correlation = xcorr(x_d,v_m,0,'coeff'); %1.4.4
 
-%% 1.3 Channel
+
+%% 1.5.2 
+% Repeat items 1.3-1.5.1, this time for FM modulation
+fd = 10^4;
+v_FM = fmmod(v_m,fc,fs,fd);
+V_FM = fftshift(fft(v_FM))/sqrt(N).';
+
+% 1.3 Channel
 N_0 = 8*10^-4;
 z = (sqrt(N_0/2)*randn(1,N)).';
-x_r = v_AM + z;
+x_r = v_FM + z;
 X_r = fftshift(fft(x_r))/sqrt(N);
 
 figure;
@@ -207,7 +353,7 @@ ylabel("X_r(f)");
 xlabel("f");
 
 
-%% 1.4 Demodulator
+% 1.4 Demodulator
 
 % 1.4.1
 x_L = bandpass(x_r,[fc-bw fc+bw],fs);
@@ -227,7 +373,7 @@ ylabel("X_L(f)");
 xlabel("f");
 
 % 1.4.2
-x_d = amdemod(x_L,fc,fs,0,k_AM);
+x_d = fmdemod(x_L,fc,fs,fd);
 X_d = fftshift(fft(lowpass(x_d,bw,fs)))/sqrt(N);
 
 % X_d1 = fftshift(fft(x_d))/sqrt(N);
@@ -253,15 +399,15 @@ legend('X_d(f)','V_m(f)');
 
 % sound(x_d,fs); %1.4.3
 
-correlation = xcorr(x_d,v_m,0,'coeff'); %1.4.4
+correlation(3) = xcorr(x_d,v_m,0,'coeff'); %1.4.4
 
-%% 1.5
+% 1.5
 % 1.5.1
 
 % 1.3 Channel
 N_0 = 0.02;
 z = (sqrt(N_0/2)*randn(1,N)).';
-x_r = v_AM + z;
+x_r = v_FM + z;
 X_r = fftshift(fft(x_r))/sqrt(N);
 
 figure;
@@ -298,7 +444,7 @@ ylabel("X_L(f)");
 xlabel("f");
 
 % 1.4.2
-x_d = amdemod(x_L,fc,fs,0,k_AM);
+x_d = fmdemod(x_L,fc,fs,fd);
 X_d = fftshift(fft(lowpass(x_d,bw,fs)))/sqrt(N);
 
 % X_d1 = fftshift(fft(x_d))/sqrt(N);
@@ -322,13 +468,12 @@ ylabel("V_m(f) and X_d(f)");
 xlabel("f");
 legend('X_d(f)','V_m(f)');
 
-sound(x_d,fs); %1.4.3
+% sound(x_d,fs); %1.4.3
 
-correlation = xcorr(x_d,v_m,0,'coeff'); %1.4.4
+correlation(4) = xcorr(x_d,v_m,0,'coeff'); %1.4.4
 
-
-
-
+%% 1.5.3
+disp(correlation);
 
 
 
